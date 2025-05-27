ecae1185e636f331520af9d03fdb00470a4fdd0b
Basketmática <tragyanway@gmail.com>
2025-05-28 00:02:26 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-# Salir si algo falla
 set -e
 
-# Instalar dependencias
-apt-get update && apt-get install -y wget unzip gnupg curl software-properties-common
+# Instalar dependencias del sistema
+apt-get update && apt-get install -y wget unzip curl gnupg
 
 # Instalar Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
 
-# Instalar Chromedriver compatible
+# Descargar chromedriver (versión compatible con Chrome)
 CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+' | head -1)
-CHROMEDRIVER_VERSION=$(curl -sS "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION}" || curl -sS https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION}" || curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 wget -O /tmp/chromedriver.zip "https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip"
 unzip /tmp/chromedriver.zip -d /usr/local/bin/
 chmod +x /usr/local/bin/chromedriver
