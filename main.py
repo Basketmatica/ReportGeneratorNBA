fb84aa8239e8016a6f202b672ffc9d88a4c42990
Basketmática <tragyanway@gmail.com>
2025-05-27 23:47:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = FastAPI()
 
 @app.get("/generate-pdf/")
 def generate_pdf(player_name: str = Query(...)):
-    temp = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
-    generar_pdf_jugador(player_name, temp.name)  # llama a tu función con el nombre
-    return FileResponse(temp.name, media_type="application/pdf", filename=f"{player_name} Report.pdf")
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp:
+        generar_pdf_jugador(player_name, temp.name)
+        temp_path = temp.name
+
+    return FileResponse(temp_path, media_type="application/pdf", filename=f"{player_name} Report.pdf")
