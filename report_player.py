2c15530f8ba5d258c7bfb3514111d3c8112dbad7
Basketmática <tragyanway@gmail.com>
2025-05-28 00:15:27 +0200

    Update report_player.py

--- report_player.py
+++ report_player.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
